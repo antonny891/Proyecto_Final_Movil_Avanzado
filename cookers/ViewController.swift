16b42697e506cc9d670e480e864f1d52ef73f3f5
MICHELLCGs <michell.condori@tecsup.edu.pe>
2024-06-17 16:47:15 -0500

    Initial Commit

---
+++ cookers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cookers
+//
+//  Created by Michell Condori on 17/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
